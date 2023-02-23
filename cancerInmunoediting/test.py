743ed7eb658540d15135818b9d60d00deaf6d188
Javier Tenorio <80603109+JavierTenorioC@users.noreply.github.com>
2023-02-23 11:24:23 -0600

    Delete test.py

--- cancerInmunoediting/test.py
+++
@@ -1,9 +0,0 @@
-import mesa
-
-from cancerInmunoediting.agents import *
-from cancerInmunoediting.model import CancerInmunoediting
-
-model = CancerInmunoediting(0.6,0.05,0.6,0.05)
-
-for i in range(10):
-    model.step()
